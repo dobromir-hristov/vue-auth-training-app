6bc6d5f2270ac3bd6d9cfe6f0d95af94f9780780
Georgi Asenov <georgi.asenov@technofy.io>
2019-04-03 17:27:19 +0300

    feat: added sweetAlert2 and implemented in Vue prorotype

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="runNotify()">Click</button>
   </div>
 </template>
+
+<script>
+export default {
+ data() {
+   return {
+
+   }
+ },
+ methods: {
+   runNotify() {
+     this.$notify.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      type: 'error',
+      confirmButtonText: 'Cool'
+    })
+   }
+ }
+}
+</script>
+
+<style>
+
+</style>
+
